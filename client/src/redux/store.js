eb97d97c68ad334d3e3bc9c18cd390c9da8b4dd5
GagenvirGill <gagenvirg@gmail.com>
2025-06-03 00:11:53 -0700

    Implemented the products page

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import inventoryReducer from './slices/inventorySlice';
+import { configureStore } from "@reduxjs/toolkit";
+import inventoryReducer from "./slices/inventorySlice";
+import productsReducer from "./slices/productsSlice";
 
 export const store = configureStore({
-    reducer: {
-        inventory: inventoryReducer,
-    }
+	reducer: {
+		inventory: inventoryReducer,
+		products: productsReducer,
+	},
 });
 
 export default store;
