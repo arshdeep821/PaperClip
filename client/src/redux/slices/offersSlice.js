d579b74f4551006f5829c0c97102f6baf75b9166
Zatt <88600035+ZatthewFan@users.noreply.github.com>
2025-06-03 07:46:32 -0700

    initial offers slice

---
+++ client/src/redux/slices/offersSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	// TODO: add dummy offers
+};
+
+export const offersSlice = createSlice({
+	name: "offers",
+	initialState,
+	reducers: {},
+});
+
+export default offersSlice.reducer;
