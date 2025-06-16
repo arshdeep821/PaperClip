9f0c66ae2b0ef6296bf78389748ca60b688b3461
Arshdeep Jaggo <arshdeep.j2003@gmail.com>
2025-06-15 23:13:16 -0700

    Changed naming convention for trade schema

--- server/models/Trade.js
+++ server/models/Trade.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose")
 
 const tradeSchema = new mongoose.Schema({
-    fromUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    toUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    itemsFrom: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true }],
-    itemsTo: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true }],
+    user1: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    user2: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    items1: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true }],
+    items2: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true }],
     status: { type: String, enum: ['pending', 'accepted', 'rejected', 'cancelled'], default: 'pending' },
 }, { timestamps: true, collection: "Trades" });
 
