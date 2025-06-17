7bdd8b15842725e1449a38bfa1518fa58d1a8fb7
GagenvirGill <gagenvirg@gmail.com>
2025-06-16 18:07:07 -0700

    Updated docker setup to work for the whole stack (client, server and db)

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		host: "0.0.0.0",	// listen on all network interfaces
-		port: 4173
+		host: "0.0.0.0", // listen on all network interfaces
+		port: 5173,
 	},
 	base: "/",
 });
--- server/server.js
+++ server/server.js
@@ -1,36 +1,37 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
 
+const testRouter = require("./routes/testRouter");
+const UserRouter = require("./routes/users");
+const CategoryRouter = require("./routes/categories");
+const ItemRouter = require("./routes/items");
 
-const testRouter = require("./routes/testRouter")
-const UserRouter = require("./routes/users")
-const CategoryRouter = require("./routes/categories")
-const ItemRouter = require("./routes/items")
-
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/test", testRouter)
-app.use("/users", UserRouter)
-app.use("/categories", CategoryRouter)
-app.use("/items", ItemRouter)
+app.use("/test", testRouter);
+app.use("/users", UserRouter);
+app.use("/categories", CategoryRouter);
+app.use("/items", ItemRouter);
 
-const port = process.env.PORT || 3001
+const port = 3001;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => {
-            console.log(`Server is listening on port ${port}`);
-        })
-    } catch (err) {
-        console.log(`Error: ${err}`);
-    }
-}
+	try {
+		await connectDB("mongodb://mongo:27017/database");
+		console.log("Connected to MongoDB");
 
-start()
+		app.listen(port, () => {
+			console.log(`Server is listening on port ${port}`);
+		});
+	} catch (err) {
+		console.log(`Error: ${err}`);
+	}
+};
+
+start();
