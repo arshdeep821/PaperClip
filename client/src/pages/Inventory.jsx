c010a70f4b3e962de46e1f0d0e5f6f6e85112cbe
Zatt <88600035+ZatthewFan@users.noreply.github.com>
2025-06-20 07:53:44 -0700

    removed TODO

--- client/src/pages/Inventory.jsx
+++ client/src/pages/Inventory.jsx
@@ -32,8 +32,6 @@ const Inventory = () => {
 		}
 
     const handleEditSubmit = async (formData) => {
-        // TODO: Dispatch action to update item
-
         const data = new FormData()
         if (formData.name) {
             data.append("name", formData.name);
