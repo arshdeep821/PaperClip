28d4fe2c12918db4247a90aa4b7a5577393ca750
GagenvirGill <gagenvirg@gmail.com>
2025-07-16 18:43:30 -0700

    Fixed a bug in the get by user1 endpoint, and updated the get by user2 to just by user

--- server/routes/trades.js
+++ server/routes/trades.js
@@ -1,20 +1,21 @@
 import { Router } from "express";
 const router = Router();
 import {
-    createTrade,
-    executeTrade,
-    getTradesByUser1Id,
-    getTradesByUser2Id,
-    updateTradeStatus,
-    getHistory,
-    getAllPendingTrades} from "../controllers/trades.js";
+	createTrade,
+	executeTrade,
+	getTradesByUser1Id,
+	getTradesByUserId,
+	updateTradeStatus,
+	getHistory,
+	getAllPendingTrades,
+} from "../controllers/trades.js";
 
 router.route("/").post(createTrade);
 router.route("/").get(getAllPendingTrades);
 router.route("/execute").patch(executeTrade);
-router.route("/history/:itemId").get(getHistory)
+router.route("/history/:itemId").get(getHistory);
 router.route("/:userId").get(getTradesByUser1Id);
-router.route("/user2/:id").get(getTradesByUser2Id);
+router.route("/user2/:id").get(getTradesByUserId);
 router.route("/:tradeId").patch(updateTradeStatus);
 
 export default router;
