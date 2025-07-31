4e45067d717a665c723bddef1c4a1fa394cbe510
GagenvirGill <gagenvirg@gmail.com>
2025-07-31 12:43:55 -0700

    Updated the help popup to have better explanations and to display the content in a more readable format

--- client/src/components/HelpModal.jsx
+++ client/src/components/HelpModal.jsx
@@ -5,57 +5,57 @@ import { Box, Modal, Typography } from "@mui/material";
 function HelpModal({ open, handleClose }) {
 	const location = useLocation();
 
-	const HOME_HELP = ``;
+	const PROFILE_HELP = [
+		"This is the profile page.",
+		"You can edit your personal details here, only you can see them.",
+		"You can set your user preferences here.",
+		"By updating your preferences our recommender will prioritize showing you those types of items.",
+	];
 
-	const PROFILE_HELP = `
-		This is the profile page.\n
-		You can edit your personal details, only you can see them.\n
-		You can set your preference of items you'd like to trade for. Our recommender will prioritize those.\n
-	`;
+	const PRODUCTS_HELP = [
+		"This is where you browse items we think you'd like based on your preferences.",
+		"You can change your preferences on the profile page!",
+		"You can go through your recommended products by using the arrow buttons on screen or your arrow keys on your keyboard!",
+		"You can hover over the items to see more details about them.",
+		"To send an offer for an item, you can click on the bag icon in the bottom right corner to and select your items to send in an offer for the current product.",
+		"Once you've selected items of yours, click on the checkmark to lock in and send the trade!",
+	];
 
-	const PRODUCTS_HELP = `
-		This is where you browse items we think you'd like. You can hover over them to see details.\n
-		Click on the bag icon to open your inventory and select items you would offer for the current product.\n
-		Click on the checkmark to lock in and send the trade!\n
-		If you'd like to see other items, use the arrow buttons or your arrow keys.\n
-	`;
+	const OFFERS_HELP = [
+		"This is where you can see offers that others have send you.",
+		"You can click on an item to see more details about it.",
+		"You can swipe through all of your pending offers that require your action with the arrow buttons.",
+		"If you don't like the current offer, you can reject it with the X button.",
+		"If you like the offer, you can accept it by clicking the checkmark, and starting a chat with the other user to dicuss the logistics regarding the swap.",
+		"You can also negotiate the offer with the ? button. You can browse and select/unselect both yours and the other's users' items for the renegotiated offer.",
+	];
 
-	const OFFERS_HELP = `
-		This is where you can see trade offers for your items.\n
-		You can click on an item to see more details.\n
-		You can see all offers you currently have with the arrow buttons.\n
-		If you don't like the current offer, you can reject it with the X button.\n
-		If you like the offer, you can accept it and discuss a meeting spot with the other person by clicking the checkmark.\n
-		You can also modify the offer with the ? button to initiate a renegotiation. You can select both yours and the other's items for the new trade.\n
-	`;
+	const CHATS_HELP = [
+		"These are the chats you currently have.",
+		"You can see accepted trades with a person in the chats.",
+		"If you both confirmed that the item has been traded, the items will be moved to your inventories.",
+	];
 
-	const CHATS_HELP = `
-		These are the chats you currently have.\n
-		You can see accepted trades with a person in the chats.\n
-		If you both confirmed that the item has been traded, the items will be moved to your inventories.\n
-	`;
+	const INVENTORY_HELP = [
+		"Here are the items in your inventory that are up for trade.",
+		"You can upload an item, edit an item, delete an item, or view its history.",
+		"You can see all your trades and their statuses by clicking the 'view trades' button.",
+	];
 
-	const INVENTORY_HELP = `
-		Here are the items in your inventory that are up for trade.\n
-		You can upload an item, edit an item, delete an item, or view its history.\n
-		You can see all your trades and their statuses by clicking the "view trades" button.\n
-	`;
+	const SEARCH_HELP = [
+		"You can search items by their names, description, condition, or owner's username.",
+		"You can also search users by their usernames.",
+		"Click on a user's card to view their inventory and details.",
+		"You can go to an item directly to offer a trade by clicking on the item card either in the search page or a user's profile.",
+		"If you'd like to chat with a user, send them a message with the chat button in their profile.",
+	];
 
-	const SEARCH_HELP = `
-		You can search items by their names, description, condition, or owner's username.\n
-		You can also search users by their usernames.\n
-		Click on a user's card to view their inventory and details.\n
-		You can go to an item directly to offer a trade by clicking on the item card either in the search page or a user's profile.\n
-		If you'd like to chat with a user, send them a message with the chat button in their profile.\n
-	`;
-
-	const SETTINGS_HELP = `
-		Here are some settings you can do.\n
-		Other users can't find your profile through the search page if you set your account to private.\n
-	`;
+	const SETTINGS_HELP = [
+		"Here are some settings you can change.",
+		"Other users can't find your profile through the search page if you set your account to private.",
+	];
 
 	const HELP_CONTENT = {
-		"/home": HOME_HELP,
 		"/profile": PROFILE_HELP,
 		"/products": PRODUCTS_HELP,
 		"/offers": OFFERS_HELP,
@@ -65,7 +65,7 @@ function HelpModal({ open, handleClose }) {
 		"/settings": SETTINGS_HELP,
 	};
 
-	const content = HELP_CONTENT[location.pathname] || "no help available for this page";
+	const content = HELP_CONTENT[location.pathname] || ["no help available for this page"];
 
 	return (
 		<Modal
@@ -80,14 +80,25 @@ function HelpModal({ open, handleClose }) {
 					variant="h6"
 					component="h2"
 					gutterBottom
+					align="center"
 				>
-					Help for {location.pathname.slice(1)}
+					Help for{" "}
+					{location.pathname.charAt(1).toUpperCase() +
+						location.pathname.slice(2)}
 				</Typography>
 				<Typography
 					id="help-modal-description"
 					variant="body1"
+					align="center"
 				>
-					{content}
+					{content.map((text) => {
+						return (
+							<>
+								<div className={styles.gap}></div>
+								{text}
+							</>
+						);
+					})}
 				</Typography>
 			</Box>
 		</Modal>
